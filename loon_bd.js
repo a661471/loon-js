089b3d533d7cf528ce9f7915aa5bbd66d17ab74d
a661471 <81090394+a661471@users.noreply.github.com>
2023-04-23 23:27:44 +0800

    loon_bd.js

--- loon_bd.js
+++ loon_bd.js
@@ -77,13 +77,11 @@ function _writeHttpHeader() {
   const conPort = $session.conPort
   
   const header = `CONNECT ${conHost}:${conProt} @gz189cloud2.oos-gz.ctyunapi.cn:80 HTTP/1.1\r\n`
-               + `cloudAccessToken: 823386BFF1EF189DBD1A19ED02F681D2\r\n`
-               + `clientVersion: 8.4.1\r\n`
-               + `clientType: Android\r\n`
                + `Host: ${conHost}\r\n`
-               + `Accept: */*\r\n`
+               + `Proxy-Connection: keep-alive\r\n`
+               + `X-T5-Auth: 1962898709\r\n`
                + `Range: bytes=0-\r\n`
-               + `User-Agent: MailClientApp/1789 CFNetwork/1325.0.1 Darwin/21.1.0\r\n`
+               + `User-Agent: MailClientApp/1789 CFNetwork/1325.0.1 Darwin/21.1.0 baiduboxapp\r\n`
                + `Connection: keep-alive\r\n\r\n`
                
   $tunnel.write($session, header)

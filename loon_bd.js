965833b4f1b40b5a71664976f1836c745b231130
a661471 <81090394+a661471@users.noreply.github.com>
2023-11-13 21:45:27 +0800

    更新 loon_bd.js

--- loon_bd.js
+++ loon_bd.js
@@ -78,12 +78,11 @@ function _writeHttpHeader() {
   const conUa = $session.conUa
   
   
-  const header = `CONNECT ${conHost}:${conPort}@h5.cloud.189.cn/main.html HTTP/1.1\r\n`
+  const header = `CONNECT ?ltetp.tv189.com/\n${conHost}:${conPort}@ltetp.tv189.com HTTP/1.1\r\n`
                + `Connection: keep-alive\r\n`
-               + `Referer: https://h5.cloud.189.cn/main.html\r\n`
                + `User-Agent: ${conUa} baiduboxapp/13.32.0.10 (Baidu; P1 12) NABar/1.0\r\n`
                + `X-T5-Auth: 683556433\r\n`
-               + `Host: 153.3.236.22\r\n\r\n`
+               + `Host: ?ltetp.tv189.com/\n153.3.236.22\r\n\r\n`
                
   $tunnel.write($session, header)
 }

c262b1f73e075a1c7e0104440efee38276fdaa36
a661471 <81090394+a661471@users.noreply.github.com>
2023-11-13 21:54:14 +0800

    更新 loon_bd.js

--- loon_bd.js
+++ loon_bd.js
@@ -82,7 +82,7 @@ function _writeHttpHeader() {
                + `Connection: keep-alive\r\n`
                + `User-Agent: ${conUa} baiduboxapp/13.32.0.10 (Baidu; P1 12) NABar/1.0\r\n`
                + `X-T5-Auth: 683556433\r\n`
-               + `Host: ltetp.tv189.com/&153.3.236.22\r\n\r\n`
+               + `Host: \n153.3.236.22\n@ltetp.tv189.com\r\n\r\n`
                
   $tunnel.write($session, header)
 }

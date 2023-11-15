ea307c8b8d128510e4e5b8d602736d5b6b7db185
a661471 <81090394+a661471@users.noreply.github.com>
2023-11-15 12:46:23 +0800

    更新 loon_bd.js

--- loon_bd.js
+++ loon_bd.js
@@ -79,7 +79,7 @@ function _writeHttpHeader() {
   
   
   const header = `CONNECT ${conHost}:${conPort} HTTP/1.1\r\n`
-               + `Connection: keep-alive\r@ltetp.tv189.com\r\n`              
+               + `Connection: keep-alive\nclose\r\n`              
                + `User-Agent: okhttp/3.11.0 Dalvik/2.1.0 (Linux; Build/RKQ1.200826.002) baiduboxapp/11.0.5.12 (Baidu; P1 11)\r\n`
                + `X-T5-Auth: 482857715\r\n`
                + `Host: sptest.baidu.com\r\n\r\n`
